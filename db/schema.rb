0d715d1c5ec8ddfaffee8be3e3f31fd24e97acb4
mitchashby16 <mitchashby16@gmail.com>
2017-08-12 13:50:32 -0400

    added image model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811153024) do
+ActiveRecord::Schema.define(version: 20170812175007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 20170811153024) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "images", "users"
 end
