af097e54d96953607706414630899111df2351cb
mitchashby16 <mitchashby16@gmail.com>
2017-08-12 13:23:14 -0400

    added login stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
+  
+  resources :users
+  get  '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
