68bc3c9522fe69c21b3f702d9bd72081a48b09e9
mitchashby16 <mitchashby16@gmail.com>
2017-08-13 18:59:15 -0400

    changed user controller permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
